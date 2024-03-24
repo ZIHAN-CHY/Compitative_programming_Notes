c7c4178997e4aecd92ecaaad1522fb1e2319afed
ZIHAN-CHY <chyzihan13@gmail.com>
2024-03-25 02:23:46 +0600

    Kruskal

--- Kruskal.cpp
+++ Kruskal.cpp
@@ -1,9 +1,23 @@
 /*Using Disjoint_set_Union*/
 #include<bits/stdc++.h>
 using namespace std;
-
-int parent[1000];
+#define ll long long
+#define sz 1000
+vector<pair<int,pair<int,int>>>adj;
+vector<int>parent(1000,-1);
+vector<bool>vis(1000,false);
 int siz[1000];
+
+void clr(){
+     // memset(vis,false,sizeof(vis));
+    // memset(parent,-1,sizeof(parent));
+    for(int i=0 ; i<sz; i++){
+        parent[i]=-1;
+        vis[i]=false;
+        
+    }
+
+}
 
 
 void make_set(int v) {
@@ -28,5 +42,36 @@ void union_sets(int a, int b) {
 
 
 int main(){
+    int n,e;
+    cin>>e>>n;
+    for(int i=0 ; i<e ; i++){
+        int u,v,w;
+        cin>>u>>v>>w;
+        adj.push_back({w,{u,v}});
+    }
+    sort(adj.begin(),adj.end());
+    for(int i=0 ; i<n ; i++){
+        make_set(i);
+    }
+    int cost=0;
+    for(int i=0; i<adj.size(); i++){
+        int wt = adj[i].first;
+        int u = adj[i].second.first;
+        int v = adj[i].second.second;
+        if(find_set(u) == find_set(v)) continue;
+        union_sets(u,v);
+        cost+=wt;
+    }
+    cout << cost << endl;
 
 }
+
+/*
+6 5
+0 2 1
+0 1 2
+2 1 1
+2 4 2
+2 3 2
+3 4 1
+*/
